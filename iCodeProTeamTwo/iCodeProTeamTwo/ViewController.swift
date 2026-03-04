c5f49248a9378c3598c74b8596f4353ec557fafc
Matin Hoshimov <matin.xoshimov.0990@mail.ru>
2026-03-04 15:01:47 +0500

    initialcommit

---
+++ iCodeProTeamTwo/iCodeProTeamTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iCodeProTeamTwo
+//
+//  Created by Hoshimov Matin on 04/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
