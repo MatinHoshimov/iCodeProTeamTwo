77af7b9cc34992d79072b6e902ad3da562ab8331
Matin Hoshimov <matin.xoshimov.0990@mail.ru>
2026-03-05 15:28:22 +0500

    FileArchitecture

---
+++ iCodeProTeamTwo/Feature /WelcomeScreen/Model/WelcomeModel.swift
@@ -0,0 +1,14 @@
+//
+//  WelcomeModel.swift
+//  iCodeProTeamTwo
+//
+//  Created by Hoshimov Matin on 04/03/26.
+//
+
+import Foundation
+
+//MARK: - Для mock данные
+
+struct WelcomeModel{
+    
+}
---
+++ iCodeProTeamTwo/Feature /WelcomeScreen/View/WelcomeView.swift
@@ -0,0 +1,15 @@
+//
+//  WelcomeView.swift
+//  iCodeProTeamTwo
+//
+//  Created by Hoshimov Matin on 04/03/26.
+//
+
+import UIKit
+
+//MARK: - Релизуем UI
+
+class WelcomeView: UIView {
+    
+
+}
