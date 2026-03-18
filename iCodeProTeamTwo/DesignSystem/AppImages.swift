5c2881d4bc4c44acc37ad4692cc6b319d43a3ecd
Madina <68954513+madina-92@users.noreply.github.com>
2026-03-18 11:31:02 +0500

    Add App images

---
+++ iCodeProTeamTwo/DesignSystem/AppImages.swift
@@ -0,0 +1,36 @@
+//
+//  AppImages.swift
+//  
+//
+//  Created by Madina Samadzoda on 18/03/26.
+//
+
+import Foundation
+
+import UIKit
+
+enum AppImages {
+
+    // MARK: - Core
+    static let search = UIImage(named: "search")
+    static let xmark = UIImage(systemName: "xmark")
+    static let arrowRight = UIImage(systemName: "arrow.right")
+    static let mockImage = UIImage(named: "mock")
+
+    // MARK: - Tabbar
+//    static let home = UIImage(named: "homeInactive")
+//    static let savedRecipes = UIImage(named: "savedRecipesInactive")
+//    static let notification = UIImage(named: "notificationInactive")
+//    static let profile = UIImage(named: "profileInactive")
+
+    static let plusButton = UIImage(named: "plusButton")
+
+    // MARK: - Onboarding
+    static let startOnboarding = UIImage(systemName: "startPage")
+    static let onboardingPageOne = UIImage(systemName: "cookPage1")
+    static let onboardingPageTwo = UIImage(systemName: "cookPage2")
+    static let onboardingPageThree = UIImage(systemName: "cookPage3")
+
+    static let bookmarkIconRed = UIImage(systemName: "bookmarkIcone")
+    static let burgerButton = UIImage(systemName: "burgerButton")
+}
